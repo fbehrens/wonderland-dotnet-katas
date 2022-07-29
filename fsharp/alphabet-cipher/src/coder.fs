6e3940d7a8d6dc7086ef136ac244385aedbec703
Frank Behrens <fbehrens@gmail.com>
2022-07-29 13:12:40 +0200

    solve alphabet cipher

--- fsharp/alphabet-cipher/src/coder.fs
+++ fsharp/alphabet-cipher/src/coder.fs
@@ -2,11 +2,46 @@
 
 module coder =
 
+    let num (c : char) = int c - 97
+
+    let zeichen (i : int) =
+        (i + 26) % 26 + 97 |> char |> string
+    // zeichen 0
+
+    let repeat (s : string) =
+        seq {
+            while (true) do
+                for c in s -> c
+        }
+
     let encode (key : string) (message : string) : string =
-        "encodeme"
+        let enc (m : char, k : char) = num m + num k |> zeichen
+        // enc ('b','b') // c
+        let ckey = repeat key
+        Seq.zip message ckey
+        |> Seq.map enc
+        |> String.concat ""
 
     let decode (key : string) (message : string) : string =
-        "decodeme"
+        let ckey = repeat key
+        let dec (m : char, k : char) = num m - num k |> zeichen
+        // dec ('c','b') // 'b'
+        Seq.zip message ckey
+        |> Seq.map dec
+        |> String.concat ""
 
     let decipher (cipher : string) (message : string) : string =
-        "decypherme"
+        let shorten (s : string) =
+            let len =
+                String.length (s)
+            seq { 0 .. len - 1 }
+            |> Seq.map (fun i -> s.[0..i])
+            |> Seq.find (fun s1 ->
+                let padded =
+                    repeat s1
+                    |> Seq.take len
+                    |> Seq.map string
+                    |> String.concat ""
+                padded = s
+            )
+        decode message cipher |> shorten
--- fsharp/alphabet-cipher/test/coder-test.fs
+++ fsharp/alphabet-cipher/test/coder-test.fs
@@ -1,31 +1,54 @@
-﻿module coder_test
+﻿module codertest
 
 open Expecto
 open alphabet_cipher.coder
 
 [<Tests>]
 let tests =
-    testList "coder" [
-        testCase "can encode a message with a secret keyword" <| fun _ ->
-            Expect.equal
-                (encode "vigilance" "meetmeontuesdayeveningatseven") "hmkbxebpxpmyllyrxiiqtoltfgzzv" ""
-            Expect.equal
-                (encode "scones" "meetmebythetree") "egsgqwtahuiljgs" ""
+    testList
+        "coder"
+        [ testCase "can encode a message with a secret keyword"
+          <| fun _ ->
+              Expect.equal
+                  (encode "vigilance" "meetmeontuesdayeveningatseven")
+                  "hmkbxebpxpmyllyrxiiqtoltfgzzv"
+                  ""
+              Expect.equal
+                  (encode "scones" "meetmebythetree")
+                  "egsgqwtahuiljgs"
+                  ""
 
-        testCase "can decode a message given an encoded message and a secret keyword" <| fun _ ->
-            Expect.equal
-                (decode "vigilance" "hmkbxebpxpmyllyrxiiqtoltfgzzv") "meetmeontuesdayeveningatseven" ""
-            Expect.equal
-                (decode "scones" "egsgqwtahuiljgs") "meetmebythetree" ""
+          testCase
+              "can decode a message given an encoded message and a secret keyword"
+          <| fun _ ->
+              Expect.equal
+                  (decode "vigilance" "hmkbxebpxpmyllyrxiiqtoltfgzzv")
+                  "meetmeontuesdayeveningatseven"
+                  ""
+              Expect.equal
+                  (decode "scones" "egsgqwtahuiljgs")
+                  "meetmebythetree"
+                  ""
 
-        testCase "can extract the secret keyword given an encrypted message and the original message" <| fun _ ->
-            Expect.equal
-                (decipher "opkyfipmfmwcvqoklyhxywgeecpvhelzg" "thequickbrownfoxjumpsoveralazydog") "vigilance" ""
-            Expect.equal
-                (decipher "hcqxqqtqljmlzhwiivgbsapaiwcenmyu" "packmyboxwithfivedozenliquorjugs") "scones" ""
-            Expect.equal
-                (decipher "hfnlphoontutufa" "hellofromrussia") "abcabcx" ""
-    ]
+          testCase
+              "can extract the secret keyword given an encrypted message and the original message"
+          <| fun _ ->
+              Expect.equal
+                  (decipher
+                      "opkyfipmfmwcvqoklyhxywgeecpvhelzg"
+                      "thequickbrownfoxjumpsoveralazydog")
+                  "vigilance"
+                  ""
+              Expect.equal
+                  (decipher
+                      "hcqxqqtqljmlzhwiivgbsapaiwcenmyu"
+                      "packmyboxwithfivedozenliquorjugs")
+                  "scones"
+                  ""
+              Expect.equal
+                  (decipher "hfnlphoontutufa" "hellofromrussia")
+                  "abcabcx"
+                  "" ]
 
 [<EntryPoint>]
 let main argv =
